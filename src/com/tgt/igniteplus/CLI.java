9959ed67bd85501c3f4ec5811aad7c4ac5fff7b7
sanmati-rm <sanmati-rm@users.noreply.github.com>
2020-04-20 14:07:17 +0530

    done

--- src/com/tgt/igniteplus/CLI.java
+++ src/com/tgt/igniteplus/CLI.java
@@ -1,183 +1,80 @@
-/*
-Command line Interface to demonstrate the use of collections
-1) User should be able to create,delete,update and display department and its employees
-2)Each and every department should have atleast one employee
-3)Department can have more than one ignite member
-*/
-
 package com.tgt.igniteplus;
+
 import java.util.*;
 
 public class CLI {
-    static List<collections> dataScience = new ArrayList<>();
-    static List<collections> infrastructure = new ArrayList<>();
-    static Set<String> skillSet1= new HashSet<>();
-    static Set<String> skillSet2 = new HashSet<>();
-    static Set<String> skillSet3 = new HashSet<>();
-    static Set<String> skillSet4 = new HashSet<>();
-    static Map<String, List<collections>> emp = new HashMap<>();
+    static List<String> dept = new ArrayList<>();
+    static Map<String,List<collections>> emp= new HashMap<>();
+    static List<collections> coll = new ArrayList<>();
+    static Set<String> skillSet = new HashSet<>();
+
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
-
-        //Set<String> skillSet1= new HashSet<>();
-        skillSet1.add("Java");
-        skillSet1.add("SQL");
-        skillSet1.add("DS");
-
-        //Set<String> skillSet2 = new HashSet<>();
-        skillSet2.add("Java");
-        skillSet2.add("NoSQL");
-        skillSet2.add("Machine Learning");
-
-        dataScience.add(new collections("Sam", "MVIT",20,skillSet1));
-
-        //Set<String> skillSet3 = new HashSet<>();
-        skillSet3.add("Linux");
-        skillSet3.add("PSQL");
-        skillSet3.add("Scripting");
-
-        //Set<String> skillSet4 = new HashSet<>();
-        skillSet4.add("Chef");
-        skillSet4.add("React");
-        skillSet4.add("AI");
-
-        infrastructure.add(new collections("Sammy", "VIT", 20, skillSet3));
-
-        int ans;
-        do {
-            int choice;
-            System.out.println("Select your choice: 1 for Adding \t 2 for Deleting \t 3 for Displaying ");
-            choice = in.nextInt();
-            switch (choice) {
-                case 1: addEmployee();
-                    break;
-                case 2: deleteEmployee();
-                    break;
-                case 3:
-                    display();
+        int ans,choice;
+        do{
+            System.out.println("Choose among the following options by pressing the appropriate number\n1:Creating a new department\n2:Deleting an existing department\n3:Displaying the details of all the employees in the departments\n4:Creating and adding a new employee into an existing department");
+            choice= in.nextInt();
+            switch(choice)
+            {
+                case 1: addDept();
+                        break;
+                case 2:deleteDept();
+                        break;
+                case 3:display();
+                        break;
+                case 4:addemp();
+                        break;
                 default:
             }
-            System.out.println("Do you want to continue: Enter 1 for Yes and Enter 0 for No");
-            ans = in.nextInt();
-        } while (ans == 1);
-
-
+            System.out.println("Do you want to continue?? Press 1 for YES and 0 for NO");
+            ans= in.nextInt();
+        }while(ans==1);
     }
-
-    private static void display() {
-        System.out.println("This is the list of existing members and the departments they belong to:");
-        Map<String, List<collections>> emp = new HashMap<>();
-        emp.put("Data Science",dataScience);
-        emp.put("Infrastructure", infrastructure);
-        for (String dept : emp.keySet()) {
-            System.out.println("Department Name: " + dept);
-            System.out.println("-----------------------------------");
-            for (collections i : emp.get(dept))
+    public static void display()
+    {
+        for(String dep: emp.keySet()){
+            System.out.println("Department name: "+ dep);
+            System.out.println("***************************************");
+            for(collections i: emp.get(dep))
                 System.out.println(i.print());
-            System.out.println("-----------------------------------");
+            System.out.println("********************************************");
         }
     }
-    private static void addEmployee() {
+    public static void addDept(){
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter name,college,age");
-        String name = in.nextLine();
-        String college = in.nextLine();
-        int age = in.nextInt();
-        System.out.println("Enter the department you want to add the employee into 1)Data Science or 2)Infrastruture");
-        int dept = in.nextInt();
-        System.out.println("Choose the skillset: 1) Java, SQL, DS  \t 2) Java,NoSQL,Machine Learning \t 3) Linux, PSQL, Scripting \t 4) Chef,React,AI");
-        int ch = in.nextInt();
-        if (dept == 1) {
-            switch (ch) {
-                case 1: dataScience.add(new collections(name,college ,age, skillSet1));
-                    break;
-                case 2:dataScience.add(new collections(name, college, age, skillSet2));
-                    break;
-                case 3:dataScience.add(new collections(name, college, age, skillSet3));
-                    break;
-                case 4:dataScience.add(new collections(name, college, age, skillSet4));
-                    break;
-                default:
-            }
-        }
-        if(dept==2)
+        System.out.println("Enter the name of the department you want to add");
+        String dep= in.nextLine();
+        emp.put(dep,null);
+        dept.add(dep);
+        System.out.println("Department added");
+    }
+    public static void deleteDept(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the name of the department you want to delete");
+        String dep = in.nextLine();
+        if(dept.contains(dep))
         {
-            switch (ch) {
-                case 1:infrastructure.add(new collections(name, college,age, skillSet1));
-                    break;
-                case 2:infrastructure.add(new collections(name, college, age, skillSet2));
-                    break;
-                case 3:infrastructure.add(new collections(name, college, age, skillSet3));
-                    break;
-                case 4:infrastructure.add(new collections(name, college, age, skillSet4));
-                    break;
-                default:
-            }
+            dept.remove(dep);
+            emp.remove(dep);
         }
-        System.out.println("Employee successfully added");
+        else
+            System.out.println("Department does not exist");
     }
-    public static void deleteEmployee(){
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter name of employee you want to delete");
-        String name = in.nextLine();
-        System.out.println("Enter the department you want to delete the employee into 1)Data Science or 2)Infrastruture");
-        int dept = in.nextInt();
-        if (dept == 1) {
-            dataScience.remove(name);
-        }
-        if(dept==2){
-            infrastructure.remove(name);
-        }
+    public static void addemp(){
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter the name of the employee");
+        String name =in.nextLine();
+        System.out.println("Enter the college of the employee");
+        String college = in.nextLine();
+        System.out.println("Enter the age of the employee");
+        int age=in.nextInt();
+        System.out.println("Enter skillset of the employee(Type done after you are done)");
+        String ss=in.nextLine();
+        skillSet.add(ss);
+        coll.add(new collections(name,college,age,skillSet));
+        System.out.println("Enter the department in which the employee should be added");
+        String dep= in.nextLine();
+        if(dept.contains(dep))
+            emp.put(dep,coll);
     }
-
 }
-
-/*
-Output:
-Select your choice: 1 for Adding 	 2 for Deleting 	 3 for Displaying
-3
-This is the list of existing members and the departments they belong to:
-Department Name: Data Science
------------------------------------
-Name: Sam
-Skill Set is [Java, SQL, DS]
------------------------------------
-Department Name: Infrastructure
------------------------------------
-Name: Sammy
-Skill Set is [Linux, PSQL, Scripting]
------------------------------------
-Do you want to continue: Enter 1 for Yes and Enter 0 for No
-1
-Select your choice: 1 for Adding 	 2 for Deleting 	 3 for Displaying
-1
-Enter name,college,age
-Sanmati
-VTU
-23
-Enter the department you want to add the employee into 1)Data Science or 2)Infrastruture
-1
-Choose the skillset: 1) Java, SQL, DS  	 2) Java,NoSQL,Machine Learning 	 3) Linux, PSQL, Scripting 	 4) Chef,React,AI
-2
-Employee successfully added
-Do you want to continue: Enter 1 for Yes and Enter 0 for No
-1
-Select your choice: 1 for Adding 	 2 for Deleting 	 3 for Displaying
-3
-This is the list of existing members and the departments they belong to:
-Department Name: Data Science
------------------------------------
-Name: Sam
-Skill Set is [Java, SQL, DS]
-Name: Sanmati
-Skill Set is [Java, NoSQL, Machine Learning]
------------------------------------
-Department Name: Infrastructure
------------------------------------
-Name: Sammy
-Skill Set is [Linux, PSQL, Scripting]
------------------------------------
-Do you want to continue: Enter 1 for Yes and Enter 0 for No
-0
- */
