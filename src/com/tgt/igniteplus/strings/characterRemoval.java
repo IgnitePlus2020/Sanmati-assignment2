4aa4d2c9d5ab87620f28f1423bd634697ad89487
Sanmati RM <46448758+sanmati-rm@users.noreply.github.com>
2020-05-01 14:23:55 +0530

    Update characterRemoval.java

--- src/com/tgt/igniteplus/strings/characterRemoval.java
+++ src/com/tgt/igniteplus/strings/characterRemoval.java
@@ -11,7 +11,6 @@ public class characterRemoval {
         System.out.println("Enter the character you want to remove");
         String ch = in.nextLine();
         int i=0;
-        str= str.replace(ch, "");
         str=str.replace(ch.tolowerCase(),"");
         str = str.replace(ch.toUpperCase(),"");
         System.out.println("The string after character removal is " + str);
