ae07be4ecde8c923c9490dc1d14aecf93bea35d7
Sanmati RM <46448758+sanmati-rm@users.noreply.github.com>
2020-05-01 14:35:58 +0530

    Update characterRemoval.java

--- src/com/tgt/igniteplus/strings/characterRemoval.java
+++ src/com/tgt/igniteplus/strings/characterRemoval.java
@@ -11,7 +11,7 @@ public class characterRemoval {
         System.out.println("Enter the character you want to remove");
         String ch = in.nextLine();
         int i=0;
-        str= str.replaceall(ch,"");
+        str= str.replaceAll(ch,"");
         System.out.println("The string after character removal is " + str);
     }
 }
