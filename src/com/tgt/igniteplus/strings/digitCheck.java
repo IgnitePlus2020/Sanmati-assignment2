0f9c6ad91357f55120841825002606999e9f0933
Sanmati RM <46448758+sanmati-rm@users.noreply.github.com>
2020-05-01 14:33:18 +0530

    Update digitCheck.java

--- src/com/tgt/igniteplus/strings/digitCheck.java
+++ src/com/tgt/igniteplus/strings/digitCheck.java
@@ -6,7 +6,6 @@ public class digitCheck {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a string");
         String str = in.nextLine();
-        int flag=0;
         System.out.println("Let's check if the string is only made of digits");
         if(str.matches("[0-9]+"))
             System.out.println("String is only made of digits");
